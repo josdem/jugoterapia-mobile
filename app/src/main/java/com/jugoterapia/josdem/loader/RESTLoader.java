fb02ce5df6dccdbe17a2fe67b31c1bc59cc5c05e
josdem <joseluis.delacruz@gmail.com>
2017-07-10 18:14:10 -0500

    #4 Deleting unused code

--- app/src/main/java/com/jugoterapia/josdem/loader/RESTLoader.java
+++ app/src/main/java/com/jugoterapia/josdem/loader/RESTLoader.java
@@ -28,11 +28,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
@@ -46,17 +42,7 @@ import android.util.Log;
 
 public class RESTLoader extends AsyncTaskLoader<RESTLoader.RESTResponse> {
     private static final String TAG = RESTLoader.class.getName();
-
-    // We use this delta to determine if our cached data is
-    // old or not. The value we have here is 10 minutes;
     private static final long STALE_DELTA = 600000;
-
-    public enum HTTPVerb {
-        GET,
-        POST,
-        PUT,
-        DELETE
-    }
 
     public static class RESTResponse {
         private String mData;
@@ -79,28 +65,14 @@ public class RESTLoader extends AsyncTaskLoader<RESTLoader.RESTResponse> {
         }
     }
 
-    private HTTPVerb     mVerb;
     private Uri          mAction;
     private Bundle       mParams;
     private RESTResponse mRestResponse;
 
     private long mLastLoad;
 
-    public RESTLoader(Context context) {
+    public RESTLoader(Context context, Uri action, Bundle params) {
         super(context);
-    }
-
-    public RESTLoader(Context context, HTTPVerb verb, Uri action) {
-        super(context);
-
-        mVerb   = verb;
-        mAction = action;
-    }
-
-    public RESTLoader(Context context, HTTPVerb verb, Uri action, Bundle params) {
-        super(context);
-
-        mVerb   = verb;
         mAction = action;
         mParams = params;
     }
@@ -108,102 +80,25 @@ public class RESTLoader extends AsyncTaskLoader<RESTLoader.RESTResponse> {
     @Override
     public RESTResponse loadInBackground() {
         try {
-            // At the very least we always need an action.
-            if (mAction == null) {
-                Log.e(TAG, "You did not define an action. REST call canceled.");
-                return new RESTResponse(); // We send an empty response back. The LoaderCallbacks<RESTResponse>
-                                           // implementation will always need to check the RESTResponse
-                                           // and handle error cases like this.
-            }
-
-            // Here we define our base request object which we will
-            // send to our REST service via HttpClient.
             HttpRequestBase request = null;
+            request = new HttpGet();
+            attachUriWithQuery(request, mAction, mParams);
 
-            // Let's build our request based on the HTTP verb we were
-            // given.
-            switch (mVerb) {
-                case GET: {
-                    request = new HttpGet();
-                    attachUriWithQuery(request, mAction, mParams);
-                }
-                break;
-
-                case DELETE: {
-                    request = new HttpDelete();
-                    attachUriWithQuery(request, mAction, mParams);
-                }
-                break;
-
-                case POST: {
-                    request = new HttpPost();
-                    request.setURI(new URI(mAction.toString()));
-
-                    // Attach form entity if necessary. Note: some REST APIs
-                    // require you to POST JSON. This is easy to do, simply use
-                    // postRequest.setHeader('Content-Type', 'application/json')
-                    // and StringEntity instead. Same thing for the PUT case
-                    // below.
-                    HttpPost postRequest = (HttpPost) request;
-
-                    if (mParams != null) {
-                        UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(paramsToList(mParams));
-                        postRequest.setEntity(formEntity);
-                    }
-                }
-                break;
-
-                case PUT: {
-                    request = new HttpPut();
-                    request.setURI(new URI(mAction.toString()));
-
-                    // Attach form entity if necessary.
-                    HttpPut putRequest = (HttpPut) request;
-
-                    if (mParams != null) {
-                        UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(paramsToList(mParams));
-                        putRequest.setEntity(formEntity);
-                    }
-                }
-                break;
-            }
-
-            if (request != null) {
-                HttpClient client = new DefaultHttpClient();
-
-                // Let's send some useful debug information so we can monitor things
-                // in LogCat.
-                Log.d(TAG, "Executing request: "+ verbToString(mVerb) +": "+ mAction.toString());
-
-                // Finally, we send our request using HTTP. This is the synchronous
-                // long operation that we need to run on this Loader's thread.
-                HttpResponse response = client.execute(request);
-
-                HttpEntity responseEntity = response.getEntity();
-                StatusLine responseStatus = response.getStatusLine();
-                int        statusCode     = responseStatus != null ? responseStatus.getStatusCode() : 0;
-
-                // Here we create our response and send it back to the LoaderCallbacks<RESTResponse> implementation.
-                RESTResponse restResponse = new RESTResponse(responseEntity != null ? EntityUtils.toString(responseEntity) : null, statusCode);
-                return restResponse;
-            }
-
-            // Request was null if we get here, so let's just send our empty RESTResponse like usual.
-            return new RESTResponse();
-        }
-        catch (URISyntaxException e) {
-            Log.e(TAG, "URI syntax was incorrect. "+ verbToString(mVerb) +": "+ mAction.toString(), e);
-            return new RESTResponse();
-        }
-        catch (UnsupportedEncodingException e) {
+            HttpClient client = new DefaultHttpClient();
+            Log.d(TAG, "Executing request: " + ": "+ mAction.toString());
+            HttpResponse response = client.execute(request);
+            HttpEntity responseEntity = response.getEntity();
+            StatusLine responseStatus = response.getStatusLine();
+            int statusCode = responseStatus != null ? responseStatus.getStatusCode() : 0;
+            RESTResponse restResponse = new RESTResponse(responseEntity != null ? EntityUtils.toString(responseEntity) : null, statusCode);
+            return restResponse;
+        } catch (UnsupportedEncodingException e) {
             Log.e(TAG, "A UrlEncodedFormEntity was created with an unsupported encoding.", e);
             return new RESTResponse();
-        }
-        catch (ClientProtocolException e) {
+        } catch (ClientProtocolException e) {
             Log.e(TAG, "There was a problem when sending the request.", e);
             return new RESTResponse();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             Log.e(TAG, "There was a problem when sending the request.", e);
             return new RESTResponse();
         }
@@ -273,24 +168,6 @@ public class RESTLoader extends AsyncTaskLoader<RESTLoader.RESTResponse> {
         catch (URISyntaxException e) {
             Log.e(TAG, "URI syntax was incorrect: "+ uri.toString());
         }
-    }
-
-    private static String verbToString(HTTPVerb verb) {
-        switch (verb) {
-            case GET:
-                return "GET";
-
-            case POST:
-                return "POST";
-
-            case PUT:
-                return "PUT";
-
-            case DELETE:
-                return "DELETE";
-        }
-
-        return "";
     }
 
     private static List<BasicNameValuePair> paramsToList(Bundle params) {
