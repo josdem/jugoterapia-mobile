6030bca3bae45494c21d8af71ea031ec651061a9
josdem <joseluis.delacruz@gmail.com>
2017-07-12 21:13:15 -0500

    #6 Deleting unused import

--- app/src/main/java/com/jugoterapia/josdem/activity/BeverageActivity.java
+++ app/src/main/java/com/jugoterapia/josdem/activity/BeverageActivity.java
@@ -23,7 +23,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
