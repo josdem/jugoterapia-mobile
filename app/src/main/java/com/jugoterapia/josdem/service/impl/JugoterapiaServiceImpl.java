e022b5c78a020d078920f7095d9b8e84769afd97
josdem <joseluis.delacruz@gmail.com>
2017-07-26 17:42:16 -0500

    #11 Adding Jugoterapia service implementation

---
+++ app/src/main/java/com/jugoterapia/josdem/service/impl/JugoterapiaServiceImpl.java
@@ -0,0 +1,56 @@
+/*
+  Copyright 2017 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.jugoterapia.josdem.service.impl;
+
+import com.jugoterapia.josdem.model.Beverage;
+import com.jugoterapia.josdem.model.Category;
+import com.jugoterapia.josdem.service.JugoterapiaService;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import retrofit2.Call;
+import retrofit2.http.Query;
+
+@Singleton
+public class JugoterapiaServiceImpl implements JugoterapiaService {
+
+  JugoterapiaService jugoterapiaService = null;
+
+  @Inject
+  public JugoterapiaServiceImpl(){
+    jugoterapiaService = JugoterapiaService.retrofit.create(JugoterapiaService.class);
+  }
+
+  @Override
+  public Call<List<Category>> getCategories() {
+    return jugoterapiaService.getCategories();
+  }
+
+  @Override
+  public Call<List<Beverage>> getBeverages(@Query("categoryId") Integer id) {
+    return jugoterapiaService.getBeverages(id);
+  }
+
+  @Override
+  public Call<Beverage> getBeverage(@Query("beverageId") Integer id) {
+    return jugoterapiaService.getBeverage(id);
+  }
+
+}
