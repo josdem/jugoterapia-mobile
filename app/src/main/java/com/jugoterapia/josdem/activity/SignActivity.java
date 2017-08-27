8d270b23decf5a10947cad72c5e0437955d78f5e
josdem <joseluis.delacruz@gmail.com>
2017-08-27 14:49:09 -0500

    #17 Adding Sign activity and view

---
+++ app/src/main/java/com/jugoterapia/josdem/activity/SignActivity.java
@@ -0,0 +1,16 @@
+package com.jugoterapia.josdem.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.jugoterapia.josdem.R;
+
+public class SignActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_sign);
+  }
+
+}
