6b45517f86a95d10647e75283e08e02696405db9
josdem <joseluis.delacruz@gmail.com>
2017-08-27 18:54:53 -0500

    #18 Adding credentials

--- app/src/main/java/com/jugoterapia/josdem/activity/SignActivity.java
+++ app/src/main/java/com/jugoterapia/josdem/activity/SignActivity.java
@@ -1,3 +1,19 @@
+/*
+  Copyright 2017 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package com.jugoterapia.josdem.activity;
 
 import android.os.Bundle;
@@ -15,7 +31,12 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.jugoterapia.josdem.R;
+import com.jugoterapia.josdem.model.Credentials;
 import com.jugoterapia.josdem.state.ApplicationState;
+
+/**
+ * @understands It handle user's authentication using Google oauth2
+ */
 
 public class SignActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
@@ -82,10 +103,19 @@ public class SignActivity extends FragmentActivity implements GoogleApiClient.On
       signInButton.setVisibility(View.GONE);
       GoogleSignInAccount account = result.getSignInAccount();
       ApplicationState.CURRENT_USER = account.getEmail();
+      sendCredentials(account);
       startCategoryActivity();
     } else {
       statusTextView.setText(getText(R.string.failure_sign));
     }
+  }
+
+  private void sendCredentials(GoogleSignInAccount account) {
+    Credentials credentials = new Credentials();
+    credentials.setName(account.getDisplayName());
+    credentials.setEmail(account.getEmail());
+    credentials.setToken(account.getIdToken());
+    //TODO: Send credentials to the server
   }
 
   private void startCategoryActivity(){
