f8b667102c0877eea2a13e3d42eb9425200b393c
josdem <joseluis.delacruz@gmail.com>
2018-08-24 16:55:19 -0400

    #20 Updating license

--- app/src/main/java/com/jugoterapia/josdem/component/ActivityComponent.java
+++ app/src/main/java/com/jugoterapia/josdem/component/ActivityComponent.java
@@ -1,3 +1,19 @@
+/*
+  Copyright 2015 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package com.jugoterapia.josdem.component;
 
 import com.jugoterapia.josdem.activity.BeverageActivity;
@@ -5,7 +21,6 @@ import com.jugoterapia.josdem.activity.CategoryActivity;
 import com.jugoterapia.josdem.activity.RecipeActivity;
 import com.jugoterapia.josdem.activity.SignActivity;
 import com.jugoterapia.josdem.context.PerActivity;
-import com.jugoterapia.josdem.model.Category;
 import com.jugoterapia.josdem.module.ActivityModule;
 
 import dagger.Component;
