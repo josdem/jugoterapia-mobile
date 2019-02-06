1887ab627bd785aac7eee53653fb2f4f2b5a0e1f
josdem <joseluis.delacruz@gmail.com>
2019-02-06 17:13:42 -0500

    #28 Changing image view name

--- app/src/main/java/com/jugoterapia/josdem/util/DownloadImageTask.java
+++ app/src/main/java/com/jugoterapia/josdem/util/DownloadImageTask.java
@@ -1,18 +1,34 @@
-package com.jugoterapia.josdem.util;
+/*
+  Copyright 2017 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.util.Log;
-import android.widget.ImageView;
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.jugoterapia.josdem.util;
 
 import java.io.InputStream;
 
-public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-  ImageView bmImage;
+import android.util.Log;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
-  public DownloadImageTask(ImageView bmImage) {
-    this.bmImage = bmImage;
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+  ImageView imageView;
+
+  public DownloadImageTask(ImageView imageView) {
+    this.imageView = imageView;
   }
 
   protected Bitmap doInBackground(String... urls) {
@@ -29,6 +45,7 @@ public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
   }
 
   protected void onPostExecute(Bitmap result) {
-    bmImage.setImageBitmap(result);
+    imageView.setImageBitmap(result);
   }
+
 }
