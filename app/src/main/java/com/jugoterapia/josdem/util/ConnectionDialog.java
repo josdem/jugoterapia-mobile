30219d3f2fe9c391793bfa45d7397d010b1c723c
josdem <joseluis.delacruz@gmail.com>
2019-03-07 08:19:50 -0500

    #34 Adding connection error value

--- app/src/main/java/com/jugoterapia/josdem/util/ConnectionDialog.java
+++ app/src/main/java/com/jugoterapia/josdem/util/ConnectionDialog.java
@@ -27,7 +27,7 @@ public class ConnectionDialog {
 
 	public static void show(final Activity activity) {
 		new AlertDialog.Builder(activity)
-		.setTitle(ApplicationState.CONNECTION_TITLE)
+		.setTitle(R.string.connection_error)
 		.setMessage(R.string.message)
 		.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog,int id){
